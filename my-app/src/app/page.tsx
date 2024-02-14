94ed095bb17ffef73fbee42762442926fa05124d
IrisKerr <iris.kerr@hotmail.com>
2024-02-14 11:16:14 +0100

    ✨get currentUser infos

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,15 @@
 import { Button, Input } from "antd";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, currentUser } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const loggedInUser = await currentUser();
+
+  const username = loggedInUser?.username
+    ? loggedInUser?.username
+    : `${loggedInUser?.firstName ?? ""}${
+        loggedInUser?.lastName ? `${loggedInUser.lastName}` : ""
+      }`;
+
   return (
     <>
       <div className="flex flex-col justify-center items-center gap-5 h-screen">
@@ -9,6 +17,9 @@ export default function Home() {
         <Button type="primary">Primary Button</Button>
         <Button>default Button</Button>
         <UserButton />
+        <span>User id : {loggedInUser?.id}</span>
+        <span>Username : {username}</span>
+        <span>Email : {loggedInUser?.emailAddresses[0].emailAddress}</span>
         <Input placeholder="basic usage" className="w-40"></Input>
       </div>
     </>
