f27b6645d2a260834448fa436b334c7ce85959ee
IrisKerr <iris.kerr@hotmail.com>
2024-02-28 17:32:52 +0100

    ✨display images from firebase - media step edit

--- my-app/src/app/(private)/user/properties/_components/properties-form/media.tsx
+++ my-app/src/app/(private)/user/properties/_components/properties-form/media.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { PropertiesFormStepProps } from ".";
 import { Button, Form, Upload } from "antd";
+import Image from "next/image";
 
 function Media({
   currentStep,
@@ -25,6 +26,26 @@ function Media({
       initialValues={finalValues.media}
       onFinish={onFinish}
     >
+      <div className="flex flex-wrap gap-5 mb-5">
+        {finalValues.media.images.map((image: string, index: any) => (
+          <div
+            key={index}
+            className="flex flex-col gap-1 border border-dashed border-gray-400 p-2 rounded justify-center items-start"
+          >
+            <Image
+              src={image}
+              alt=""
+              className="object-cover rounded"
+              width={40}
+              height={40}
+              placeholder="blur"
+            />
+            <span className="text-red-500 underline cursor-pointer">
+              delete
+            </span>
+          </div>
+        ))}
+      </div>
       <Upload
         listType="picture-card"
         multiple
