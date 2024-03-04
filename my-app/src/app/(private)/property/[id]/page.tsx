935fb1693cd2c4836722d2a5296fc53de9079ffe
IrisKerr <iris.kerr@hotmail.com>
2024-03-04 15:04:53 +0100

    ✨PropertyDetailsPage complete

--- my-app/src/app/(private)/property/[id]/page.tsx
+++ my-app/src/app/(private)/property/[id]/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { findPropertyById } from "@/actions/properties";
+import LinkButton from "@/app/components/link-button";
 import PageTitle from "@/app/components/page-title";
 import { Property } from "@prisma/client";
+import { Carousel, Divider } from "antd";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 
@@ -13,6 +15,42 @@ interface Props {
 
 function PropertyPage({ params: { id } }: Props) {
   const [property, setProperty] = useState<Property>({});
+
+  function capitalizeWords(str: string) {
+    if (typeof str === "string") {
+      return str
+        .split(" ")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+    } else {
+      return "";
+    }
+  }
+
+  const getAttributeDetails = ({
+    name,
+    value,
+  }: {
+    name: string;
+    value: any;
+  }) => {
+    return (
+      <div className="flex justify-between">
+        <span className="text-sm text-rgay-600">{name}</span>
+        <span className="text-sm text-rgay-600">{value}</span>
+      </div>
+    );
+  };
+
+  const getSectionTitle = (title: string) => {
+    return (
+      <div>
+        <h3 className="text-xl font-bold text-primary m-0">{title}</h3>
+        <hr className="border border-solid border-gray-300 my-3" />
+      </div>
+    );
+  };
+
   useEffect(() => {
     const fetchProperties = async () => {
       try {
@@ -29,16 +67,112 @@ function PropertyPage({ params: { id } }: Props) {
 
   return (
     <div>
-      <PageTitle title={property.name} />
-      {property.images && property.images.length > 0 && (
-        <Image
-          className="object-cover w-800 h-500 rounded-t"
-          src={property.images?.[0]}
-          alt=""
-          width={800}
-          height={500}
-        />
-      )}
+      <div className="flex items-center justify-between">
+        <h2 className="text-2xl lg:text-3xl font-bold text-primary">
+          {capitalizeWords(property?.name)}
+        </h2>
+        <div>
+          <LinkButton title="Back to properties" path="/" />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-10">
+        <div className="col-span-2">
+          {property.images && property.images.length > 0 && (
+            <Carousel autoplay autoplaySpeed={2800}>
+              {property.images.map((image, index) => (
+                <div key={index}>
+                  <Image
+                    className="object-cover w-full h-96 lg:h-[550px] rounded-md"
+                    src={image}
+                    alt={image}
+                    width={1200}
+                    height={500}
+                  />
+                </div>
+              ))}
+            </Carousel>
+          )}
+          <div className="text-sm text-gray-600 py-4 text-justify">
+            {property.description}
+          </div>
+        </div>
+
+        <div className="col-span-1 border border-solid border-gray-300 rounded p-5">
+          <div className="flex flex-col">{getSectionTitle("Amenities")}</div>
+          <div className="flex flex-col gap-1">
+            {getAttributeDetails({
+              name: "bedrooms",
+              value: property.bedrooms,
+            })}
+            {getAttributeDetails({
+              name: "bathrooms",
+              value: property.bathrooms,
+            })}
+            {getAttributeDetails({
+              name: "floors",
+              value: property.floors,
+            })}
+            {getAttributeDetails({
+              name: "area",
+              value: `${property.area} m²`,
+            })}
+            {getAttributeDetails({
+              name: "furnishing",
+              value: property.furnishing,
+            })}
+            {getAttributeDetails({
+              name: "price",
+              value: `${property.price} -.€`,
+            })}
+            {getAttributeDetails({
+              name: "status",
+              value: property.status,
+            })}
+          </div>
+          <div className="flex flex-col mt-7">{getSectionTitle("Address")}</div>
+          <div className="flex flex-col gap-1">
+            {getAttributeDetails({
+              name: "Town",
+              value: property.town,
+            })}
+            {getAttributeDetails({
+              name: "Zipcode",
+              value: property.pincode,
+            })}
+            {getAttributeDetails({
+              name: "Country",
+              value: property.country,
+            })}
+            {getAttributeDetails({
+              name: "Adress",
+              value: property.address,
+            })}
+          </div>
+
+          {property.showOwnerContact && (
+            <div>
+              <div className="flex flex-col mt-7">
+                {getSectionTitle("Owner Details")}
+              </div>
+              <div className="flex flex-col gap-1">
+                {getAttributeDetails({
+                  name: "Owner Name",
+                  value: property.ownerName,
+                })}
+                {getAttributeDetails({
+                  name: "Owner Email",
+                  value: property.ownerEmail,
+                })}
+                {getAttributeDetails({
+                  name: "Owner PhoneNumber",
+                  value: property.ownerPhoneNumber,
+                })}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
