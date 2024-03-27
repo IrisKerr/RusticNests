e98d42601e038d9b8176ac4883b0cd15ad3d4f63
IrisKerr <iris.kerr@hotmail.com>
2024-03-27 15:09:14 +0100

    ✨generate Stripe client secret key

---
+++ my-app/src/actions/payments.ts
@@ -0,0 +1,18 @@
+"use server";
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY!);
+
+export const getStripeClientSecretKey = async (amount: number) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount * 100,
+      currency: "eur",
+      description: "Payment for RusticNests",
+    });
+    // return client secret key
+    return {
+      clientSecret: paymentIntent.client_secret,
+    };
+  } catch (error: any) {
+    return { error: error.message };
+  }
+};
