1b79765df47ac381d022ca7c935f2cc0249d8184
IrisKerr <iris.kerr@hotmail.com>
2024-05-06 14:35:49 +0200

    ✨AdminPropertiesPage : manage properties

--- my-app/src/app/(private)/admin/properties/page.tsx
+++ my-app/src/app/(private)/admin/properties/page.tsx
@@ -1,9 +1,38 @@
-import React from "react";
+"use client";
+import {
+  findAllProperties,
+  findPropertiesByFilters,
+} from "@/actions/properties";
+import Filters from "@/app/components/filters";
+import PageTitle from "@/app/components/page-title";
+import { Property } from "@prisma/client";
+import React, { useEffect } from "react";
+import ClientSidePropertiesTable from "../../user/properties/_components/properties-form/properties-table-clientside";
 
-function AdminPropertiesPage() {
+function AdminPropertiesPage({ searchParams }: { searchParams: string }) {
+  const [properties, setProperties] = React.useState<Property[]>([]);
+  useEffect(() => {
+    const fetchAllProperties = async () => {
+      try {
+        if (!searchParams) {
+          const result = await findAllProperties();
+          setProperties(result?.data || []);
+        }
+        const result = await findPropertiesByFilters(searchParams);
+        setProperties(result?.data || []);
+      } catch (error) {
+        console.error("An error occurred while requesting the database", error);
+      }
+    };
+
+    fetchAllProperties();
+  }, [searchParams]);
+
   return (
     <>
-      <div>admin properties page</div>
+      <PageTitle title="Manage Properties" />
+      <Filters searchParams={searchParams} />
+      <ClientSidePropertiesTable properties={properties} />
     </>
   );
 }
